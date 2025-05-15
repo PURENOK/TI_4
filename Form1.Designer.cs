865ca94a13940568fe23f0a99c5fc1a4f7f1151f
PURENOK <lkek31321@gmail.com>
2025-05-15 14:12:10 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace TI_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            OpenFileButton = new Button();
+            SignatureFileButton = new Button();
+            GenerateSign = new Button();
+            CheckSign = new Button();
+            SaveSignFile = new Button();
+            textBoxQ = new TextBox();
+            TextBoxH = new TextBox();
+            textBoxX = new TextBox();
+            textBoxK = new TextBox();
+            textBoxP = new TextBox();
+            TextBoxForHash = new TextBox();
+            textBoxForSign = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            textBoxFileContent = new TextBox();
+            SuspendLayout();
+            // 
+            // OpenFileButton
+            // 
+            OpenFileButton.Location = new Point(174, 164);
+            OpenFileButton.Name = "OpenFileButton";
+            OpenFileButton.Size = new Size(108, 54);
+            OpenFileButton.TabIndex = 0;
+            OpenFileButton.Text = "Открыть файл";
+            OpenFileButton.UseVisualStyleBackColor = true;
+            OpenFileButton.Click += OpenFileButton_Click;
+            // 
+            // SignatureFileButton
+            // 
+            SignatureFileButton.Location = new Point(303, 164);
+            SignatureFileButton.Name = "SignatureFileButton";
+            SignatureFileButton.Size = new Size(121, 54);
+            SignatureFileButton.TabIndex = 1;
+            SignatureFileButton.Text = "Открыть подписанный файл";
+            SignatureFileButton.UseVisualStyleBackColor = true;
+            SignatureFileButton.Click += SignatureFileButton_Click;
+            // 
+            // GenerateSign
+            // 
+            GenerateSign.Location = new Point(32, 164);
+            GenerateSign.Name = "GenerateSign";
+            GenerateSign.Size = new Size(108, 54);
+            GenerateSign.TabIndex = 2;
+            GenerateSign.Text = "Сгенерировать подпись";
+            GenerateSign.UseVisualStyleBackColor = true;
+            GenerateSign.Click += GenerateSign_Click;
+            // 
+            // CheckSign
+            // 
+            CheckSign.Location = new Point(445, 164);
+            CheckSign.Name = "CheckSign";
+            CheckSign.Size = new Size(108, 54);
+            CheckSign.TabIndex = 3;
+            CheckSign.Text = "Проверить подпись";
+            CheckSign.UseVisualStyleBackColor = true;
+            CheckSign.Click += CheckSign_Click;
+            // 
+            // SaveSignFile
+            // 
+            SaveSignFile.Location = new Point(174, 224);
+            SaveSignFile.Name = "SaveSignFile";
+            SaveSignFile.Size = new Size(108, 54);
+            SaveSignFile.TabIndex = 4;
+            SaveSignFile.Text = "Сохранить подписанный файл";
+            SaveSignFile.UseVisualStyleBackColor = true;
+            SaveSignFile.Click += SaveSignFile_Click;
+            // 
+            // textBoxQ
+            // 
+            textBoxQ.Location = new Point(32, 43);
+            textBoxQ.Name = "textBoxQ";
+            textBoxQ.Size = new Size(100, 23);
+            textBoxQ.TabIndex = 5;
+            // 
+            // TextBoxH
+            // 
+            TextBoxH.Location = new Point(191, 43);
+            TextBoxH.Name = "TextBoxH";
+            TextBoxH.Size = new Size(100, 23);
+            TextBoxH.TabIndex = 6;
+            // 
+            // textBoxX
+            // 
+            textBoxX.Location = new Point(191, 94);
+            textBoxX.Name = "textBoxX";
+            textBoxX.Size = new Size(100, 23);
+            textBoxX.TabIndex = 7;
+            // 
+            // textBoxK
+            // 
+            textBoxK.Location = new Point(353, 43);
+            textBoxK.Name = "textBoxK";
+            textBoxK.Size = new Size(100, 23);
+            textBoxK.TabIndex = 8;
+            // 
+            // textBoxP
+            // 
+            textBoxP.Location = new Point(32, 94);
+            textBoxP.Name = "textBoxP";
+            textBoxP.Size = new Size(100, 23);
+            textBoxP.TabIndex = 9;
+            // 
+            // TextBoxForHash
+            // 
+            TextBoxForHash.Location = new Point(32, 321);
+            TextBoxForHash.Multiline = true;
+            TextBoxForHash.Name = "TextBoxForHash";
+            TextBoxForHash.ReadOnly = true;
+            TextBoxForHash.Size = new Size(132, 106);
+            TextBoxForHash.TabIndex = 10;
+            // 
+            // textBoxForSign
+            // 
+            textBoxForSign.Location = new Point(191, 321);
+            textBoxForSign.Multiline = true;
+            textBoxForSign.Name = "textBoxForSign";
+            textBoxForSign.ReadOnly = true;
+            textBoxForSign.Size = new Size(132, 106);
+            textBoxForSign.TabIndex = 11;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(32, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(16, 15);
+            label1.TabIndex = 12;
+            label1.Text = "Q";
+           
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(32, 76);
+            label2.Name = "label2";
+            label2.Size = new Size(14, 15);
+            label2.TabIndex = 13;
+            label2.Text = "P";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(191, 25);
+            label3.Name = "label3";
+            label3.Size = new Size(16, 15);
+            label3.TabIndex = 14;
+            label3.Text = "H";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(191, 76);
+            label4.Name = "label4";
+            label4.Size = new Size(14, 15);
+            label4.TabIndex = 15;
+            label4.Text = "X";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(353, 25);
+            label5.Name = "label5";
+            label5.Size = new Size(14, 15);
+            label5.TabIndex = 16;
+            label5.Text = "K";
+            // 
+            // textBoxFileContent
+            // 
+            textBoxFileContent.Location = new Point(353, 321);
+            textBoxFileContent.Multiline = true;
+            textBoxFileContent.Name = "textBoxFileContent";
+            textBoxFileContent.ReadOnly = true;
+            textBoxFileContent.Size = new Size(132, 106);
+            textBoxFileContent.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBoxFileContent);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBoxForSign);
+            Controls.Add(TextBoxForHash);
+            Controls.Add(textBoxP);
+            Controls.Add(textBoxK);
+            Controls.Add(textBoxX);
+            Controls.Add(TextBoxH);
+            Controls.Add(textBoxQ);
+            Controls.Add(SaveSignFile);
+            Controls.Add(CheckSign);
+            Controls.Add(GenerateSign);
+            Controls.Add(SignatureFileButton);
+            Controls.Add(OpenFileButton);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button OpenFileButton;
+        private Button SignatureFileButton;
+        private Button GenerateSign;
+        private Button CheckSign;
+        private Button SaveSignFile;
+        private TextBox textBoxQ;
+        private TextBox TextBoxH;
+        private TextBox textBoxX;
+        private TextBox textBoxK;
+        private TextBox textBoxP;
+        private TextBox TextBoxForHash;
+        private TextBox textBoxForSign;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox textBoxFileContent;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.IO;
+using System.Numerics;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TI_4
+{
+    public partial class Form1 : Form
+    {
+        private string fileContent = "";
+        private string signedFilePath = "";
+        private BigInteger r = BigInteger.Zero;
+        private BigInteger s = BigInteger.Zero;
+        private const int DefaultH0 = 100; // Константа вместо параметра по умолчанию
+        private string filePath = ""; // Для хранения пути к текущему файлу
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Хеш-функция по методичке (без параметра по умолчанию)
+        private BigInteger CalculateHash(string message, BigInteger p, BigInteger q)
+        {
+            // Вычисляем n = p*q (аналог константы 323 в Swift-коде)
+            BigInteger n = p * q;
+            BigInteger hash = DefaultH0; // Начальное значение H0 = 100
+
+            foreach (char c in message)
+            {
+                BigInteger charValue = (int)c; // Получаем ASCII-код символа
+                hash = BigInteger.ModPow(hash + charValue, 2, n);
+            }
+
+            return hash % q; // Финальное взятие по модулю q
+        }
+
+        private int CharToNumber(char c)
+        {
+            // A=1, B=2, ..., Z=26 (только английские)
+            if (c >= 'A' && c <= 'Z') return c - 'A' + 1;
+            if (c >= 'a' && c <= 'a') return c - 'a' + 1;
+            return 0; // Для остальных
+        }
+
+        // Быстрое возведение в степень по модулю
+        private BigInteger ModExp(BigInteger number, BigInteger exponent, BigInteger modulus)
+        {
+            return BigInteger.ModPow(number, exponent, modulus);
+        }
+
+        // Обратный элемент по малой теореме Ферма
+        private BigInteger ModInverse(BigInteger a, BigInteger q)
+        {
+            return ModExp(a, q - 2, q); // Малая теорема Ферма
+        }
+
+        // Проверка на простоту (упрощенная)
+        private bool IsPrime(BigInteger number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            var boundary = (BigInteger)Math.Floor(Math.Exp(BigInteger.Log(number) / 2));
+
+            for (BigInteger i = 3; i <= boundary; i += 2)
+            {
+                if (number % i == 0) return false;
+            }
+
+            return true;
+        }
+
+        private void OpenFileButton_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = openFileDialog.FileName;
+                    fileContent = File.ReadAllText(filePath, Encoding.UTF8);
+                    textBoxFileContent.Text = fileContent;
+                    MessageBox.Show("Файл успешно загружен", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void GenerateSign_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Получаем параметры
+                BigInteger q = BigInteger.Parse(textBoxQ.Text);
+                BigInteger p = BigInteger.Parse(textBoxP.Text);
+                BigInteger n = p * q;
+
+                BigInteger h = BigInteger.Parse(TextBoxH.Text);
+                BigInteger x = BigInteger.Parse(textBoxX.Text);
+                BigInteger k = BigInteger.Parse(textBoxK.Text);
+
+                // Проверки параметров
+                if (!IsPrime(q)) throw new ArgumentException("q должно быть простым числом");
+                if (!IsPrime(p)) throw new ArgumentException("p должно быть простым числом");
+                if ((p - 1) % q != 0) throw new ArgumentException("q должно быть делителем (p-1)");
+                if (x <= 0 || x >= q) throw new ArgumentException("x должно быть в интервале (0, q)");
+                if (k <= 0 || k >= q) throw new ArgumentException("k должно быть в интервале (0, q)");
+
+                // Вычисляем g
+                BigInteger g = ModExp(h, (p - 1) / q, p);
+                if (g <= 1) throw new ArgumentException("g должно быть больше 1");
+
+                // Для пустого файла fileContent будет "", но хеш все равно вычислится
+                BigInteger hash = CalculateHash(fileContent ?? "", p, q);
+                TextBoxForHash.Text = hash.ToString();
+
+                // Вычисляем подпись
+                do
+                {
+                    r = ModExp(g, k, p) % q;
+                    if (r == 0)
+                    {
+                        MessageBox.Show("r = 0, введите другое значение k", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        textBoxK.Text = "";
+                        return;
+                    }
+
+                    BigInteger kInv = ModInverse(k, q);
+                    s = (kInv * (hash + x * r)) % q;
+
+                    if (s == 0)
+                    {
+                        MessageBox.Show("s = 0, введите другое значение k", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        textBoxK.Text = "";
+                        return;
+                    }
+                } while (r == 0 || s == 0);
+
+                textBoxForSign.Text = $"r = {r}\ns = {s}";
+                MessageBox.Show("Подпись успешно сгенерирована", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void SaveSignFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (r == 0 || s == 0)
+                {
+                    throw new ArgumentException("Сначала сгенерируйте подпись");
+                }
+
+                using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                {
+                    saveFileDialog.FileName = string.IsNullOrEmpty(filePath)
+                        ? "signed_file.txt"
+                        : Path.GetFileNameWithoutExtension(filePath) + "_signed" + Path.GetExtension(filePath);
+
+                    saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        signedFilePath = saveFileDialog.FileName;
+                        File.WriteAllText(signedFilePath, $"{fileContent}\n---SIGNATURE---\n{r}\n{s}", Encoding.UTF8);
+                        textBoxFileContent.Text = fileContent; // Обновляем содержимое
+                        MessageBox.Show("Подписанный файл успешно сохранен", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void SignatureFileButton_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    signedFilePath = openFileDialog.FileName;
+                    string content = File.ReadAllText(signedFilePath, Encoding.UTF8);
+                    textBoxFileContent.Text = content; // Показываем содержимое файла
+                    MessageBox.Show("Подписанный файл успешно загружен", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void CheckSign_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Получаем параметры
+                BigInteger q = BigInteger.Parse(textBoxQ.Text);
+                BigInteger p = BigInteger.Parse(textBoxP.Text);
+                BigInteger h = BigInteger.Parse(TextBoxH.Text);
+                BigInteger x = BigInteger.Parse(textBoxX.Text);
+
+                // Проверки параметров
+                if (!IsPrime(q)) throw new ArgumentException("q должно быть простым числом");
+                if (!IsPrime(p)) throw new ArgumentException("p должно быть простым числом");
+                if ((p - 1) % q != 0) throw new ArgumentException("q должно быть делителем (p-1)");
+
+                // Вычисляем g
+                BigInteger g = ModExp(h, (p - 1) / q, p);
+                if (g <= 1) throw new ArgumentException("g должно быть больше 1");
+
+                // Вычисляем открытый ключ y
+                BigInteger y = ModExp(g, x, p);
+
+                if (string.IsNullOrEmpty(signedFilePath) || !File.Exists(signedFilePath))
+                {
+                    throw new ArgumentException("Сначала загрузите подписанный файл");
+                }
+
+                string[] fileLines = File.ReadAllLines(signedFilePath, Encoding.UTF8);
+
+                // Ищем индекс строки с разделителем подписи
+                int signatureIndex = -1;
+                for (int i = 0; i < fileLines.Length; i++)
+                {
+                    if (fileLines[i] == "---SIGNATURE---")
+                    {
+                        signatureIndex = i;
+                        break;
+                    }
+                }
+
+                if (signatureIndex == -1 || signatureIndex >= fileLines.Length - 2)
+                {
+                    throw new ArgumentException("Файл не содержит подпись в правильном формате");
+                }
+
+                // Извлекаем сообщение и подпись
+                string message = string.Join("\n", fileLines, 0, signatureIndex);
+                BigInteger fileR = BigInteger.Parse(fileLines[signatureIndex + 1]);
+                BigInteger fileS = BigInteger.Parse(fileLines[signatureIndex + 2]);
+
+                // Вычисляем хеш сообщения (работает и для пустой строки)
+                BigInteger hash = CalculateHash(message, p, q);
+
+                // Проверяем подпись
+                BigInteger w = ModInverse(fileS, q);
+                BigInteger u1 = (hash * w) % q;
+                BigInteger u2 = (fileR * w) % q;
+                BigInteger v = (ModExp(g, u1, p) * ModExp(y, u2, p)) % p % q;
+
+                // Выводим результаты
+                string result = v == fileR ? "Подпись верна" : "Подпись неверна";
+                string details = $"Вычисленные значения:\nw = {w}\nu1 = {u1}\nu2 = {u2}\nv = {v}\nr из файла = {fileR}";
+
+                MessageBox.Show($"{result}\n\n{details}", "Результат проверки", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            // Пустая реализация
+        }
+    }
+}
